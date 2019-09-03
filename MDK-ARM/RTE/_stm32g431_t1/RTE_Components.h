8a7edbfd9df4c41922dfdf9bb0aa839506ab37b7
zhanzr <zhanzr@foxmail.com>
2019-09-03 23:14:34 +0800

    First STM32G431 project generated by STM32CubeMX

---
+++ MDK-ARM/RTE/_stm32g431_t1/RTE_Components.h
@@ -0,0 +1,30 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32g431_t1' 
+ * Target:  'stm32g431_t1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g4xx.h"
+
+/*  Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:TTY:ITM:1.2.0 */
+#define RTE_Compiler_IO_TTY             /* Compiler I/O: TTY */
+          #define RTE_Compiler_IO_TTY_ITM         /* Compiler I/O: TTY ITM */
+
+
+#endif /* RTE_COMPONENTS_H */
